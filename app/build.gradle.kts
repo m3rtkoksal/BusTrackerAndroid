56a73fa60b6c9e1189c584807a4e8951cad1fe7f
Mert Koksal <mert.koksal@machinarium.co>
2026-06-05 00:22:10 +0300

    vers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.mikatechnology.BusTracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.3"
+        versionCode = 10
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
